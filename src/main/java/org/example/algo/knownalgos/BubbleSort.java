63019125b65002c63cab330fffb7d9eaf9582bae
rovshan <rouvsen@gmail.com>
2023-05-14 23:03:26 +0400

    Bubble Sort added

--- src/main/java/org/example/algo/knownalgos/BubbleSort.java
+++ src/main/java/org/example/algo/knownalgos/BubbleSort.java
@@ -4,11 +4,13 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] sortedArr = bubbleSort(new int[]{3, 1, 4, 2, 7, -2});
+//        int[] sortedArr = bubbleSort(new int[]{3, 1, 4, 2, 7, -2});
+        int[] sortedArr = bubbleSort(new int[]{1,2,3});
         System.out.println(Arrays.toString(sortedArr));
     }
     public static int[] bubbleSort(int[] arr) {//bubble?
         //logic here
+        boolean isSorted = true;
         int temp;
         for (int key = 0; key < arr.length-1; key++) {
             for (int nextVal = key+1; nextVal < arr.length; nextVal++) {
@@ -17,9 +19,14 @@ public class BubbleSort {
                     temp = arr[key];
                     arr[key] = arr[nextVal];
                     arr[nextVal] = temp;
+                    isSorted = false;
                 }
             }
             //i=key, j=nextVal
+            if (isSorted) {
+                System.out.println("Already Sorted");
+                return arr;
+            }
         }
         return arr;
     }
