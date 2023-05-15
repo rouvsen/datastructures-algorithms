9ee0e720d210143ed2730e19629acb6706fa0382
rovshan <rouvsen@gmail.com>
2023-05-15 13:32:30 +0400

    Insertion Sort added

--- src/main/java/org/example/algo/knownalgos/InsertionSort.java
+++ src/main/java/org/example/algo/knownalgos/InsertionSort.java
@@ -8,24 +8,17 @@ public class InsertionSort {
         System.out.println(Arrays.toString(sortedArr));
     }
 
-    private static int[] insertionSortPractice(int[] arr) {
-        //logic here, write :)
-
-        return arr;
-    }
-
     private static int[] insertionSort(int[] arr) {
-        //logic here, look
-        for (int i = 1; i < arr.length; ++i) {
-            int nextArg = arr[i];//second arg.
-            int firstArg = i - 1;//first arg.
-            while (firstArg >= 0 && arr[firstArg] > nextArg) {//first > second
-                arr[firstArg + 1] = arr[firstArg];//
-                firstArg = firstArg - 1;//j=-1
+        //logic here
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];//2nd arg
+            int j = key - 1;//first index
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;//j--;
             }
-            arr[firstArg + 1] = nextArg;//
+            arr[j + 1] = key;//j = (-1), -1 + 1 = 0, 0th index = second arg
         }
-        //write like this
         return arr;
     }
 }
