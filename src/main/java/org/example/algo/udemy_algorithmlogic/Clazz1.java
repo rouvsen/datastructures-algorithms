44d64ec68de230030ba0f5cc8ff14baaa015f49c
rovshan <rouvsen@gmail.com>
2023-06-27 17:58:02 +0400

    find factorial of all numbers between two numbers

--- src/main/java/org/example/algo/udemy_algorithmlogic/Clazz1.java
+++ src/main/java/org/example/algo/udemy_algorithmlogic/Clazz1.java
@@ -5,7 +5,8 @@ public class Clazz1 {
     public static void main(String[] args) {
 //        squareOfNumbersBetweenXandY(5, 10);//all are inclusive
 //        findFactorialSimpleV1(5);
-        System.out.println(findFactorialSimpleV2(5));
+//        System.out.println(findFactorialSimpleV2(5));
+        findFactorialForGivenNumbersButTheirsAmong(5, 10);
     }
 
     public static void squareOfNumbersBetweenXandY(int num1, int num2) {
@@ -31,6 +32,23 @@ public class Clazz1 {
         if(num == 0)
             return 1;
         return num * findFactorialSimpleV2(num - 1);
+    }
+
+    public static void findFactorialForGivenNumbersButTheirsAmong(int num1, int num2) {
+        int period = num1;
+
+        while(period <= num2) {
+            //for each Period
+            int counter = 1;
+            int number = period;
+            while (number > 1) {
+                counter *= number--;
+            }
+            System.out.println("factorial of " + number + " is " + counter);
+
+            period++;
+        }
+
     }
 
 }
