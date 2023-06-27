08cb337ab71ca1a213223240d352f4e01a9c7643
rovshan <rouvsen@gmail.com>
2023-06-27 17:24:35 +0400

    find factorial - simple way added

--- src/main/java/org/example/algo/udemy_algorithmlogic/Clazz1.java
+++ src/main/java/org/example/algo/udemy_algorithmlogic/Clazz1.java
@@ -1,9 +1,12 @@
 package org.example.algo.udemy_algorithmlogic;
 
 public class Clazz1 {
+
     public static void main(String[] args) {
-        squareOfNumbersBetweenXandY(5, 10);//all are inclusive
+//        squareOfNumbersBetweenXandY(5, 10);//all are inclusive
+        findFactorialSimpleV1(5);
     }
+
     public static void squareOfNumbersBetweenXandY(int num1, int num2) {
         int counter = 0;
         for (int i = num1; i <= num2; i++) {
@@ -13,4 +16,14 @@ public class Clazz1 {
         System.out.println("square of all nums and sum - between " + num1 + " and " + num2
                 + ", result: " + counter);
     }
+
+    public static void findFactorialSimpleV1(int num) {
+        int counter = 1;//5
+        int number = num;
+        while (num > 1) {
+            counter *= num--;
+        }
+        System.out.println("factorial of " + number + " is " + counter);
+    }
+
 }
