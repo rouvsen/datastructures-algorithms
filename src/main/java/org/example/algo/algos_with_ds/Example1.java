7a5526aa0737672be00b2391eca0d70366692974
rovshan <rouvsen@gmail.com>
2023-10-30 12:22:23 +0400

    Add  algorighm to the project

--- src/main/java/org/example/algo/algos_with_ds/Example1.java
+++ src/main/java/org/example/algo/algos_with_ds/Example1.java
@@ -1,8 +1,34 @@
 package org.example.algo.algos_with_ds;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Example1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        //Find letters count in the word
+
+        String s = "Azerbaycan";
+
+        s = s.toLowerCase();
+
+        char[] arr = s.toCharArray();
+
+        int count = 1;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : arr) {
+            if (!map.containsKey(c)) {
+                map.put(c, count);
+            } else {
+                int val = map.get(c);
+                val++;
+                map.put(c, val);
+            }
+        }
+
+        System.out.println(map);
     }
 }
